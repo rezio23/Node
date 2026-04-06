0a1a99ae8a230ffb719d5936233bfca587a1a6e1
R3zio <vichheansombath@gmail.com>
2026-04-06 15:01:25 +0700

    Initial backend tutorial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+Tutorial Learning from freeCodeCamp.org
---
+++ backend/src/app.js
@@ -0,0 +1,5 @@
+import express from "express"; // Setup express
+
+const app = express(); // Create an express app
+
+export default app; // Declare global use
